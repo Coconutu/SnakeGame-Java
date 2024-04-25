d239ac28ce3646da2773935f1de52ae75386a082
Coconutu <sebastian.daniel.adrian@gmail.com>
2024-04-25 22:58:48 +0300

    creating snakegame

--- src/main/java/com/daniel/App.java
+++ src/main/java/com/daniel/App.java
@@ -14,5 +14,12 @@ public class App
         int boardHeight=boardWidth;
         JFrame frame=new JFrame("Snake");
         frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null); //face fereastra sa se deschida in centrul ecranului
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame=new SnakeGame(boardWidth,boardHeight);
+        frame.add(snakeGame);
     }
 }
---
+++ src/main/java/com/daniel/SnakeGame.java
@@ -0,0 +1,17 @@
+package com.daniel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SnakeGame extends JPanel {
+    int boardWidth;
+    int boardHeight;
+    SnakeGame(int boardWidth,int boardHeight){
+        this.boardWidth=boardWidth;
+        this.boardHeight=boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth,this.boardHeight));
+        setBackground(Color.BLACK);
+
+    }
+
+}
