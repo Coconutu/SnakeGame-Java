5c8116c1b0d84b2b4044e70b6bc637f68ddb1874
Coconutu <sebastian.daniel.adrian@gmail.com>
2024-04-25 22:40:13 +0300

    Initial commit

---
+++ src/main/java/com/daniel/App.java
@@ -0,0 +1,18 @@
+package com.daniel;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int boardWidth=600;
+        int boardHeight=boardWidth;
+        JFrame frame=new JFrame("Snake");
+        frame.setVisible(true);
+    }
+}
